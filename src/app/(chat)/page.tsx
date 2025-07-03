466562441809cff2a3d34b75612663cda68a43d7
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-07-03 10:32:25 +0300

    fix(Page): adjust layout to ensure full height for chat page

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-5">
+    <div className="flex h-full flex-col items-center justify-center gap-5">
       <h1 className="text-xl md:text-2xl">What can I help with?</h1>
       <ChatInput onSubmit={handleChatCreation} />
     </div>
