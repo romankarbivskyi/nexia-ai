8d1af64d0c70e052b28403dca37c5298c80cd756
romankarbivskyi <romankarbivskyi1@gmail.com>
2025-07-04 12:05:24 +0300

    fix(auth): refactor GET function to improve request handling and code clarity

--- src/app/auth/callback/route.tsx
+++ src/app/auth/callback/route.tsx
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  let next = searchParams.get("next") ?? "/";
-  if (!next.startsWith("/")) {
-    next = "/";
-  }
+  const next = searchParams.get("next") ?? "/";
+  const origin = request.nextUrl.origin;
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
+
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
